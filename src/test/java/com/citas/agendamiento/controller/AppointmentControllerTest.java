7a974722b305bb564f1a805d9bea74b6a736f4fc
leonardoMontealegre <leomontealegre001@gmail.com>
2022-12-10 08:19:36 -0500

    creacion de controladores de tests y apointments

---
+++ src/test/java/com/citas/agendamiento/controller/AppointmentControllerTest.java
@@ -0,0 +1,28 @@
+package com.citas.agendamiento.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppointmentControllerTest {
+
+    @Test
+    void getAllAppointments() {
+    }
+
+    @Test
+    void getAppointmentById() {
+    }
+
+    @Test
+    void addAppointment() {
+    }
+
+    @Test
+    void updateAppointment() {
+    }
+
+    @Test
+    void deletedAppointment() {
+    }
+}
---
+++ src/test/java/com/citas/agendamiento/controller/TestsControllerTest.java
@@ -0,0 +1,29 @@
+package com.citas.agendamiento.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestsControllerTest {
+
+    @Test
+    void getAllTests() {
+    }
+
+    @Test
+    void getTestsById() {
+
+    }
+
+    @Test
+    void addTest() {
+    }
+
+    @Test
+    void updateTest() {
+    }
+
+    @Test
+    void deletedTest() {
+    }
+}

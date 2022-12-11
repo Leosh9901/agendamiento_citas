d0e1d9421a9eb53061eb80ce1dfbec318a1e20a7
leonardoMontealegre <leomontealegre001@gmail.com>
2022-12-11 14:51:44 -0500

    Realizacion primer teste getallaffiliate

--- src/test/java/com/citas/agendamiento/controller/AffiliatesControllerTest.java
+++ src/test/java/com/citas/agendamiento/controller/AffiliatesControllerTest.java
@@ -1,56 +1,84 @@
 package com.citas.agendamiento.controller;
 
-import com.citas.agendamiento.AgendamientoApplication;
 import com.citas.agendamiento.entity.Affiliate;
-import com.citas.agendamiento.repository.AffiliateRepository;
 import com.citas.agendamiento.service.AffiliateService;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.List;
 
+import static org.hamcrest.Matchers.is;
+
+@WebMvcTest(AffiliatesController.class)
 class AffiliatesControllerTest {
 
-
-
+    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
     @Autowired
-    Affiliate affiliate;
+    private MockMvc mockMvc;
 
+    private Affiliate affiliateOne;
 
-    AffiliateRepository affiliateRepository = Mockito.mock(AffiliateRepository.class);
+    private Affiliate affiliateTwo;
 
-    @Autowired
-    AffiliatesController affiliatesController = new AffiliatesController();
+    @MockBean
+    private AffiliateService affiliateService;
 
     @BeforeEach
     void setUp() {
-
-
-
-        Mockito.when(affiliatesController.getAllAffiliate());
-    }
-
-
-    @Test
-    void getAllAffiliate() {
+        //Given =Arrange
+        affiliateOne = new Affiliate(2, "nicolas", 22, "nico@gmail.com");
+        affiliateTwo = new Affiliate(3, "luis", 27, "luis@gmail.com");
     }
 
     @Test
-    void getAffiliateByUd() {
+    void getAllAffiliate() throws Exception {
+        //Data
+        List<Affiliate> affiliateList = new ArrayList<>();
+        affiliateList.add(affiliateOne);
+        affiliateList.add(affiliateTwo);
+
+        //When
+        Mockito.when(affiliateService.getAllAffiliates()).thenReturn(affiliateList);
+
+        //Then = Assert
+        mockMvc.perform(
+                MockMvcRequestBuilders.get("/affiliates/allAffiliates")
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(
+                MockMvcResultMatchers.status().is(200)
+        ).andExpect(
+                MockMvcResultMatchers.jsonPath("$[1].age", is(27))
+        );
+    }
+
+    @Test
+    void getAffiliateById() {
+        // 1
     }
 
     @Test
     void addAffiliate() {
+     //   affiliate = new Affiliate("sdvsd", 12, "dssldkns");
     }
 
     @Test
     void updateAffiliate() {
+      //  affiliate = new Affiliate(1, "sdvsd", 12, "dssldkns");
     }
 
     @Test
     void deletedAffiliate() {
+        // 1
     }
+
+
 }
